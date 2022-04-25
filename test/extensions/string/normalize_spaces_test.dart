8cc4147854f84a841d658d54260c0ce33cc23f0a
Miguel Angel Besalduch Garcia <mabg.dev@gmail.com>
2022-04-25 10:39:11 -0300

    source StackTrace extension

--- test/extensions/string/normalize_spaces_test.dart
+++ test/extensions/string/normalize_spaces_test.dart
@@ -3,7 +3,7 @@ import 'package:test/test.dart';
 import 'package:dart_utils/extensions/string/normalize_spaces.dart';
 
 void main() {
-  test('Normalize spaces String extension', () {
+  test('String normalizeSpaces extension', () {
     var cad = '  Hello  world ';
     expect(cad.normalizeSpaces(), 'Hello world');
   });
