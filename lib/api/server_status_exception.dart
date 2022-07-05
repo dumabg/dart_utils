ba0ad9a9223427841426c58366a1b804298a1371
Miguel Angel Besalduch Garcia <miguel.besalduch@indulgeme.app>
2022-07-05 18:25:54 -0300

    ServerStatusException isUpgradeRequired

--- lib/api/server_status_exception.dart
+++ lib/api/server_status_exception.dart
@@ -2,5 +2,6 @@ class ServerStatusException implements Exception {
   int status;
   String? reasonPhrase;
   bool isUnauthorized() => status == 401;
+  bool isUpgradeRequired() => status == 426;
   ServerStatusException(this.status, this.reasonPhrase);
 }
