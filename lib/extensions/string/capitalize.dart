2e78935ba7df6984684ad5a79cf085ced7fb2990
Miguel Angel Besalduch Garcia <mabg.dev@gmail.com>
2023-08-07 18:33:02 +0200

    Changed Capitalize extension capitalizeFirstofEach to capitalizeFirstOfEach

--- lib/extensions/string/capitalize.dart
+++ lib/extensions/string/capitalize.dart
@@ -1,5 +1,5 @@
 extension Capitalize on String {
   String get capitalizeFirst => '${this[0].toUpperCase()}${substring(1)}';
-  String get capitalizeFirstofEach =>
+  String get capitalizeFirstOfEach =>
       split(' ').map((str) => str.capitalizeFirst).join(' ');
 }
