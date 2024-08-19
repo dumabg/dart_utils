e5cafcfb10498ff9c665cfe85cd69f98eb227a1f
Miguel Angel Besalduch Garcia <mabg.dev@gmail.com>
2024-08-19 12:11:42 -0300

    ApiProtoc getHeaders

--- lib/api/api_protoc.dart
+++ lib/api/api_protoc.dart
@@ -3,11 +3,10 @@ import 'package:http/http.dart' as $http;
 import 'package:protobuf/protobuf.dart' as $pb;
 import 'api_config.dart';
 import 'server_status_exception.dart';
-//import 'package:http2/transport.dart';
-//import 'dart:io';
 
 class ApiProtoc {
   static Future<String?> Function()? getToken;
+  static Map<String, String>? Function()? getHeaders;
 
   static Future<Uint8List> call($pb.GeneratedMessage message) async {
     String messageName = message.info_.messageName;
@@ -16,6 +15,9 @@ class ApiProtoc {
           messageName.substring(0, messageName.length - 'Request'.length);
     }
     Map<String, String>? headers;
+    if (getHeaders != null) {
+      headers = getHeaders!();
+    }
     if (getToken != null) {
       final String? idToken = await getToken!();
       if (idToken != null) {
@@ -27,58 +29,6 @@ class ApiProtoc {
     if (api != null) {
       path = '/$api$path';
     }
-    //   return await _http2(headers, message, path);
-    // }
-
-    // static Future<Uint8List> _http2(Map<String, String>? headers,
-    //     $pb.GeneratedMessage message, String path) async {
-    //   String urlScheme = ApiConfig.urlScheme;
-    //   bool useSSL = urlScheme == 'https';
-    //   String ulrHost = ApiConfig.ulrHost;
-    //   Socket socket;
-    //   if (useSSL) {
-    //     socket = await SecureSocket.connect(ulrHost, ApiConfig.urlPort,
-    //         supportedProtocols: ['h2']);
-    //     if ((socket as SecureSocket).selectedProtocol != 'h2') {
-    //       throw Exception('Failed to negotiate http/2 via alpn. Maybe server '
-    //           "doesn't support http/2.");
-    //     }
-    //   } else {
-    //     socket = await Socket.connect(ulrHost, ApiConfig.urlPort);
-    //   }
-    //   socket.add(message.writeToBuffer());
-
-    //   ClientTransportConnection transport =
-    //       ClientTransportConnection.viaSocket(socket);
-    //   List<Header> headers = [
-    //     Header.ascii(':method', 'POST'),
-    //     Header.ascii(':path', path),
-    //     Header.ascii(':scheme', urlScheme),
-    //     Header.ascii(':authority', ulrHost),
-    //   ];
-
-    //   ClientTransportStream stream =
-    //       transport.makeRequest(headers, endStream: true);
-
-    //   List<int>? data;
-    //   await for (StreamMessage message in stream.incomingMessages) {
-    //     if (message is HeadersStreamMessage) {
-    //       // for (Header header in message.headers) {
-    //       //   var name = utf8.decode(header.name);
-    //       //   var value = utf8.decode(header.value);
-    //       //   print('$name: $value');
-    //       // }
-    //     } else if (message is DataStreamMessage) {
-    //       // Use [message.bytes] (but respect 'content-encoding' header)
-    //       data = message.bytes;
-    //     }
-    //   }
-    //   await transport.finish();
-    //   return Uint8List.fromList(data ?? List.empty());
-    // }
-
-    // static Uint8List _http(Map<String, String>? headers,
-    //     $pb.GeneratedMessage message, String path) {
     return $http
         .post(
             Uri(
